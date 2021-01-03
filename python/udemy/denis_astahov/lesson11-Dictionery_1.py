3e415f67ec16f0134cbcc28ad65c09584b70e0ce
lastfreenick <lastfreenick@yandex.ru>
2021-01-03 11:05:00 +0300

    add directory for DA

---
+++ python/udemy/denis_astahov/lesson11-Dictionery_1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+enemy = {
+        'loc_x': 70,
+        'loc_y': 50,
+        'color': 'green',
+        'health': 100,
+        'name': 'Mudilo',
+}
+
+print(enemy)
+
+print('location X =', enemy['loc_x'])
+print('location Y =', enemy['loc_y'])
+print('His name is:', enemy['name'])
+
+enemy['rank'] = 'Admiral'
+print(enemy)
+
+del enemy['rank']
+print(enemy)
+
+enemy['loc_x'] += 40
+enemy['health'] -= 30
+
+if enemy['health'] < 80:
+    enemy['color'] = 'yellow'
+
+print(enemy)
+
+print(enemy.keys())
+print(enemy.values())
