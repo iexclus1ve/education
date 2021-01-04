69b0952426d982d7aab7e637d0dc748667531310
iexclus1ve <lastfreenick@yandex.ru>
2021-01-04 15:39:57 +0300

    change encoding

--- python/udemy/denis_astahov/lesson19-Exceptions.py
+++ python/udemy/denis_astahov/lesson19-Exceptions.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-filename = 'about'
-myfile = open(filename, mode='r', encoding='Latin-1')
+filename = 'about.txt'
+myfile = open(filename, mode='r', encoding='utf-8')
 print(myfile.read())
