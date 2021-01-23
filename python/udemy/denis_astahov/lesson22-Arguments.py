662b838d901b3fd6b88304b84b233f289f5989e5
lastfreenick <lastfreenick@yandex.ru>
2021-01-23 13:36:19 +0300

    add lesson22

--- python/udemy/denis_astahov/lesson22-Arguments.py
+++ python/udemy/denis_astahov/lesson22-Arguments.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+import sys
+import os
 print('Hello')
+
+print(sys.argv)
+
+x = len(sys.argv)
+
+if x > 1:
+    if sys.argv[1] == '/?':
+        print('Help Requested')
+        print('Usage of this program is: myfile.py arg1 arg2 arg3')
+    print('Arguments entered: ', str(sys.argv[1:]))
+else:
+    print('Argiments not provided')
+
+os.system('ls -lah > log22.log')
+sys.exit()
