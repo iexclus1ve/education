25df63e77a816117e099602f622212288a528a6b
lastfreenick <lastfreenick@yandex.ru>
2021-02-07 10:49:02 +0300

    add turtle3.py - butterfly

--- python/mycode/turtle3.py
+++ python/mycode/turtle3.py
@@ -1,3 +1,19 @@
 import turtle
+turtle.speed(10)
 
-turtle.forward(100)
+
+def eternity(x, y):
+    for i in range(180):
+        turtle.forward(x)
+        turtle.left(y)
+    for i in range(180):
+        turtle.forward(x)
+        turtle.right(y)
+
+
+turtle.left(90)
+d = 2
+for eternity_count in range(10):
+    eternity(d, 2)
+    d += 0.2
+turtle.mainloop()
