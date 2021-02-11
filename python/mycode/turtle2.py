437919a06ed3a3866bd5292325d2955eb12c7a69
LastFreeNick <lastfreenick@yandex.ru>
2021-02-11 16:46:18 +0300

    create flower with def and for

--- python/mycode/turtle2.py
+++ python/mycode/turtle2.py
@@ -1,27 +1,23 @@
 import turtle
-turtle.speed(100)
+turtle.speed(20)
+turtle.shape('turtle')
+corner = 360
 
-for i in range(180):
-    turtle.forward(2)
-    turtle.left(2)
-for i in range(180):
-    turtle.forward(2)
-    turtle.right(2)
 
-turtle.left(60)
-for i in range(180):
-    turtle.forward(2)
-    turtle.left(2)
-for i in range(180):
-    turtle.forward(2)
-    turtle.right(2)
+def turtle_circle():
+    for i in range(180):
+        turtle.forward(2)
+        turtle.left(2)
+    for i in range(180):
+        turtle.forward(2)
+        turtle.right(2)
 
-turtle.left(45)
-for i in range(180):
-    turtle.forward(2)
-    turtle.left(2)
-for i in range(180):
-    turtle.forward(2)
-    turtle.right(2)
+
+for i in range (3):
+    turtle.left(corner)
+    turtle_circle()
+    corner /= 6
+    if corner < 45:
+        corner = 45
 
 turtle.mainloop()
