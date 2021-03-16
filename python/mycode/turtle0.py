a3f01ca0ef01775767b3796b49f7ba2ecf615885
lastfreenick <lastfreenick@yandex.ru>
2021-03-16 10:13:01 +0300

    add function

--- python/mycode/turtle0.py
+++ python/mycode/turtle0.py
@@ -3,13 +3,19 @@ turtle.shape('turtle')
 turtle.pensize(3)
 turtle.color('purple', 'green')
 #turtle.speed(20)
-for step in range(6):
-    turtle.begin_fill()
-    for i in range(3):
+
+
+def david():
+    for step in range(6):
+        turtle.begin_fill()
+        for i in range(3):
+            turtle.forward(50)
+            turtle.right(360 / 3)
+        turtle.end_fill()
         turtle.forward(50)
-        turtle.right(360 / 3)
-    turtle.end_fill()
-    turtle.forward(50)
-    turtle.left(360 / 6)
+        turtle.left(360 / 6)
+
+
+david()
 turtle.hideturtle()
 turtle.mainloop()
