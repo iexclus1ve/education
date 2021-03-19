dd224f41a5902b82fd666e09eebed948f6affb7a
lastfreenick <lastfreenick@yandex.ru>
2021-03-19 14:33:11 +0300

    add somsing interesting

--- python/mycode/turtle0.py
+++ python/mycode/turtle0.py
@@ -1,8 +1,13 @@
 import turtle
+import math
 turtle.shape('turtle')
 turtle.pensize(3)
 turtle.color('red', 'black')
-# turtle.speed(20)
+turtle.speed(3)
+c = 240
+r = c / 2 * math.pi
+d = r * 2
+l = (2 * r) * math.sin(108 * math.pi / 180)
 
 
 def david():
@@ -18,18 +23,32 @@ def david():
 
 def star():
     turtle.penup()
-    turtle.goto(-24, 0)
     turtle.pendown()
-    turtle.begin_fill()
-    turtle.left(72)
-    turtle.forward(144)
+    # turtle.begin_fill()
+    turtle.left(108)
+    turtle.forward(l)
     for i in range(4):
         turtle.right(144)
-        turtle.forward(144)
+        turtle.forward(l)
+    # turtle.end_fill()
+
+
+def circle():
+    turtle.begin_fill()
+    turtle.penup()
+    turtle.goto(0, -r)
+    turtle.pendown()
+    turtle.circle(r)
+    turtle.penup()
+    # turtle.home()
+    # turtle.goto(0, r)
+    turtle.pendown()
     turtle.end_fill()
-
-
-david()
+circle()
 star()
+turtle.penup()
+turtle.home()
+turtle.pendown()
+david()
 turtle.hideturtle()
 turtle.mainloop()
