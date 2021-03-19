e9fd58adb9cf14bc08245fa79f46fefd5db5032c
lastfreenick <lastfreenick@yandex.ru>
2021-03-19 10:02:18 +0300

    create def star

--- python/mycode/turtle0.py
+++ python/mycode/turtle0.py
@@ -1,7 +1,7 @@
 import turtle
 turtle.shape('turtle')
 turtle.pensize(3)
-turtle.color('purple', 'green')
+turtle.color('red', 'black')
 # turtle.speed(20)
 
 
@@ -9,13 +9,27 @@ def david():
     for step in range(6):
         turtle.begin_fill()
         for i in range(3):
-            turtle.forward(50)
+            turtle.forward(36)
             turtle.right(360 / 3)
         turtle.end_fill()
-        turtle.forward(50)
+        turtle.forward(36)
         turtle.left(360 / 6)
 
 
+def star():
+    turtle.penup()
+    turtle.goto(-24, 0)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.left(72)
+    turtle.forward(144)
+    for i in range(4):
+        turtle.right(144)
+        turtle.forward(144)
+    turtle.end_fill()
+
+
 david()
+star()
 turtle.hideturtle()
 turtle.mainloop()
