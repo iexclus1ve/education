846fa404eb87d46ee6643cd823b36117068cddd9
iexclus1ve <lastfreenick@yandex.ru>
2021-03-22 17:07:19 +0300

    add first code in js

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+// Type of Data
+
+// Примитивные данные
+console.log(2021, 1.5);  // Integer
+console.log(20 * 'String'); // NaN
+console.log("String");  // String
+console.log(1 / 0);  // Infinity
+console.log("Hello", "Hello", `Hello`); // String
+console.log(true, false);  // Bulean
+console.log(null);  // Null
+console.log(undefined);  // Undefined
+console.log(Symbol());  // Symbol
+
+// Ссылочные данные
+console.log({ name: "Denis", age: 30 })  // Object
+console.log([ 1, 3, 4, 4 ])  // Object
