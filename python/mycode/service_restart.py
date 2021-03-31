48b4e7ccb4a798f5c86008fa2eb54985bc861c14
iexclus1ve <lastfreenick@yandex.ru>
2021-03-31 10:53:28 +0300

    minor update code, add getpass

--- python/mycode/service_restart.py
+++ python/mycode/service_restart.py
@@ -1,21 +1,29 @@
 # /usr/bin/env python
 
 import paramiko
+import getpass
 
 client = paramiko.SSHClient()
 
+commandStart = 'sudo systemctl start openvpn@server'
+commandStop = 'sudo systemctl stop openvpn@server'
+host = input('Enter IP address: ')
+port = 22
+username = input('Enter username: ')
+password = getpass.getpass('Enter password: ')
 
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect('192.168.1.39', 22, 'support', 'usergatepass')
+client.connect(host, port, username, password)
 client.get_transport()
+print('Connection Success!')
 
-command = "sudo systemctl start openvpn@server"
+command = commandStart
 stdin, stdout, stderr = client.exec_command(command=command, get_pty=True)
-#print("command sent!")
-stdin.write("usergatepass\n")
-#print("Password sent!")
+print("command sent!")
+stdin.write(password + '\n')
+print("Password sent!")
 stdin.flush()
-#print("Input flushed!")
+print("Input flushed!")
 
 if stderr.channel.recv_exit_status() != 0:
     print("Error occured!")
