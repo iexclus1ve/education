b089aacb592f51469a66210156e9f73800a651db
iexclus1ve <lastfreenick@yandex.ru>
2021-03-31 11:57:31 +0300

    add more variables

--- python/mycode/service_restart.py
+++ python/mycode/service_restart.py
@@ -5,10 +5,19 @@ import getpass
 
 client = paramiko.SSHClient()
 
-commandStart = 'sudo systemctl start openvpn@server'
-commandStop = 'sudo systemctl stop openvpn@server'
-host = input('Enter IP address: ')
-port = 22
+control = 'systemctl'
+serviceName = 'openvpn@server'
+actionStart = 'start'
+actionStop = 'stop'
+promt = 'sudo'
+
+
+commandStart = f'{promt} {control} {actionStart} {serviceName}'
+commandStop = f'{promt} {control} {actionStop} {serviceName}'
+
+host = input('Enter IP address or hostname: ')
+port = int(input('Enter port number (default 22): '))
+
 username = input('Enter username: ')
 password = getpass.getpass('Enter password: ')
 
@@ -17,7 +26,20 @@ client.connect(host, port, username, password)
 client.get_transport()
 print('Connection Success!')
 
-command = commandStart
+
+command = int(input('Enter 1 for START service or 0 for STOP service: '))
+
+for i in range(3):
+    if command == 1:
+        command = commandStart
+        print('Service will be start')
+    elif command == 0:
+        command = commandStop
+        print('Service will be stop')
+    else:
+        print('Enter a correct value')
+
+
 stdin, stdout, stderr = client.exec_command(command=command, get_pty=True)
 print("command sent!")
 stdin.write(password + '\n')
